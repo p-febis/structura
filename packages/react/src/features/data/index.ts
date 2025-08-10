f4004f2301c8bb38c3d4e490f3a5a8259beddf28
p-febis <preston.febis@yahoo.com>
2025-08-10 07:10:49 -0300

    react: re-export the 'useCreateOne' hook

--- packages/react/src/features/data/index.ts
+++ packages/react/src/features/data/index.ts
@@ -1,7 +1,9 @@
 import { useList } from "./hooks/use-list";
 import { useOne } from "./hooks/use-one";
+import { useCreateOne } from "./hooks/use-create-one";
 
 export {
   useList,
-  useOne
+  useOne,
+  useCreateOne,
 }
